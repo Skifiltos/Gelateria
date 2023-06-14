6428d61d9432bf9a8f4a111e82f5aaadda52bbda
badtype <g.c.faina@gmail.com>
2023-06-14 11:52:23 +0200

    singolo prodotto

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,7 +6,36 @@ import data from "../fakeData";
 const url = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
 
 const Menu = () => {
-  return <div className="container">MENU</div>;
+
+  const [prodotti, setProdotti ] = React.useState(data);
+  
+  const categorie = Array.from(new Set(prodotti.map((el) => el.categoria)));
+
+  categorie.unshift ("all");
+
+  return <div className="container">
+    <h4 style={{texalign: "center", textTransform: "uppercase"}}>
+      Le nostre scelte
+    </h4>
+    <div className="lista-categorie">
+      {
+        categorie.map((categoria, index) => {
+          return <button key={index} className="btn btn-selector">
+            {categoria}
+          </button>
+        })
+      }
+    </div>
+
+    <div className="vetrina">
+      {
+        prodotti.map((el) => (
+          <Gelato key={el.id} {...el} />
+        ))
+      }
+    </div>
+
+  </div>;
 };
 
 export default Menu;
