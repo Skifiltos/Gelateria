e2e2df7b36742b5f8a8c1da6d832ffd700cf0e91
badtype <g.c.faina@gmail.com>
2023-06-20 23:23:38 +0200

    data fetching

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,51 +5,161 @@ import data from "../fakeData";
 
 const url = "https://react--course-api.herokuapp.com/api/v1/data/gelateria";
 
-const Menu = () => {
+const Menu = () => 
+{
 
-  const [prodotti, setProdotti ] = React.useState(data);
+  // Loading State for data fetching
+  const [isLoading, setIsLoading] = useState(true);
+
+  // Error Handling State
+  const [isError, setIsError] = useState(false);
+
+  // Tutti i prodotti
+  const [prodotti, setProdotti ] = React.useState();
+
+  // Active BTN Selector
   const [selected, setSelected] = useState(0);
-  const [filterProducts, setFilterProducts] = useState(prodotti);
-  const categorie = Array.from(new Set(prodotti.map((el) => el.categoria)));
 
-  categorie.unshift ("all");
+  // Products Filtered from the prodotti state
+  const [filterProducts, setFilterProducts] = useState();
 
-  const filterProdotti = (categoria, index) => {
+  // Categorie di prodotti
+  const [categorie, setCategorie] = useState([]);
+  
+  // const categorie = Array.from(new Set(prodotti.map((el) => el.categoria)));
+  // categorie.unshift ("all");
+
+  const filterProdotti = (categoria, index) => 
+  {
     setSelected(index);
 
-    if (categoria === "all") {
-      setFilterProducts(data);
-    } else {
-      setFilterProducts(prodotti.filter(el => el.categoria === categoria ? el : ""));
+    if (categoria === "all") 
+    {
+      setFilterProducts(prodotti);
+    }
+    else
+    {
+      setFilterProducts(
+        prodotti.filter((el) => (el.categoria === categoria ? el : ""))
+      );                                      
     }
   }
 
-  return <div className="container">
-    <h4 style={{texalign: "center", textTransform: "uppercase"}}>
+  React.useEffect(
+    () => 
+      {
+
+      // Funzione Eseguita Immediatamente
+        (async () => 
+            {
+                setIsLoading(true);
+                setIsError(false);
+                try
+                {
+                  const response = await axios.get(url);
+                  setProdotti(response.data.data);
+                  setFilterProducts(response.data.data);
+                  console.log(response.data.data.default);
+
+                  // Categorie Array
+                  const nuoveCategorie = Array.from(
+                    new Set(response.data.data.map((el) => el.categoria))
+                  );
+
+                  nuoveCategorie.unshift("all");
+
+
+                  setCategorie(nuoveCategorie);
+                  
+                  setIsLoading(false);
+                  setIsError(false);
+                }
+                catch (error) 
+                {
+                  console.log(error);
+                  setIsLoading(false);
+                  setIsError(true);
+                }
+            }
+        )
+        ();
+      },[]
+    );
+
+  return (
+    <div className="container">
+     <h4 style={{texalign: "center", textTransform: "uppercase"}}>
       Le nostre scelte
-    </h4>
-    <div className="lista-categorie">
+      </h4>
       {
-        categorie.map((categoria, index) => {
-          return <button 
-            key={index} 
-            onClick={() => filterProdotti(categoria, index)}
-            className={`btn btn-selector ${index === selected && "active"}`}>
-            {categoria}
-          </button>
-        })
+        !isLoading && !isError ? (
+          <>
+              <div className="lista-categorie">
+                {
+                  categorie.map((categoria, index) => 
+                    {
+                      return (
+                          <button 
+                            key={index} 
+                            onClick={() => filterProdotti(categoria, index)}
+                            className=
+                            {
+                              `btn btn-selector $
+                              {
+                                index === selected && "active"
+                              }`
+                            }
+                          >
+                          {categoria}
+                        </button>
+                      );
+                    }
+                  )
+                
+                }
+              </div>
+              <div className="vetrina">
+              {
+                filterProducts.map((el) => (
+                    <Gelato key={el.id} {...el} />
+                  )
+                )
+              }
+            </div>
+          </>
+        ) : !isLoading && isError  ? (
+          <h4
+            style=
+            {
+              {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)"
+              } 
+            }
+          >
+          Error...
+          </h4> 
+        ) : (
+          <h4 
+            style=
+            {
+              {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)"
+              }
+            }
+          >
+          Loading...
+          </h4>
+        )
       }
-    </div>
-
-    <div className="vetrina">
-      {
-        filterProducts.map((el) => (
-          <Gelato key={el.id} {...el} />
-        ))
-      }
-    </div>
-
-  </div>;
+  </div>
+  );
 };
+
 
 export default Menu;
